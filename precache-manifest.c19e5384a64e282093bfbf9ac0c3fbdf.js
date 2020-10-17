79a7ce032a342097f80995e3ac99e9d2061044ae
shaikmohammed9009 <shaikmohammed9009@gmail.com>
2020-10-17 12:35:55 +0530

    Updates

---
+++ precache-manifest.c19e5384a64e282093bfbf9ac0c3fbdf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a18533f88ee56e75a4d55ce926934285",
+    "url": "/md/index.html"
+  },
+  {
+    "revision": "18c56cfadca922555987",
+    "url": "/md/static/css/main.32517f1c.chunk.css"
+  },
+  {
+    "revision": "71f46b05a6285990ab55",
+    "url": "/md/static/js/2.e7373450.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/md/static/js/2.e7373450.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "18c56cfadca922555987",
+    "url": "/md/static/js/main.69efab7c.chunk.js"
+  },
+  {
+    "revision": "f4d5ce92638b60527c3c",
+    "url": "/md/static/js/runtime-main.5cf4ef15.js"
+  }
+]);
